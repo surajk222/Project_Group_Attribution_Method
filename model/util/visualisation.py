c0cdb7ccd6fab5917771c5b0ecfd6527083b6966
mrschmdt <leon.schmidt33@gmail.com>
2023-08-31 14:06:49 +0100

    Added visualisation method

---
+++ model/util/visualisation.py
@@ -0,0 +1,37 @@
+from matplotlib import pyplot as plt
+import numpy as np
+
+def visualise_loss_and_accuracy(
+        train_loss: list[float],
+        validation_loss: list[float],
+        train_accuracy: list[float],
+        validation_accuracy: list[float]
+    ) -> None:
+    """
+    Visualises train- and validation loss- and accuracy during training.
+
+    Args:
+        train_loss(list[float]): List of the train losses.
+        validation_loss(list[float]): List of the validation losses.
+        train_accuracy(list[float]): List of train accuracies.
+        validation_accuracy(list[float]): List of validation accuracies.
+    """
+    x = np.linspace(0, len(train_loss),num=len(train_loss))
+
+    fig, (ax1,ax2) = plt.subplots(2)
+
+    
+    ax1.plot(x,train_loss, label="Train-Loss")
+    ax1.plot(x,validation_loss, label="Validation-Loss")
+    ax1.set_xlabel("Epoch")
+    ax1.set_ylabel("Cross-Entropy Loss")
+    ax1.legend()
+
+    ax2.plot(x, train_accuracy, label="Train-Accuracy")
+    ax2.plot(x,validation_accuracy, label="Validation-Accuracy")
+    ax2.set_xlabel("Epoch")
+    ax2.set_ylabel("Accuracy")
+    ax2.legend()
+
+    plt.tight_layout()
+    plt.show()
