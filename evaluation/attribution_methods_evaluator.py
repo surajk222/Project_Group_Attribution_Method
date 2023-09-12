6443187b55e9aecbd71b595363b44dedaaab9f30
mrschmdt <leon.schmidt33@gmail.com>
2023-09-12 14:10:30 +0200

    random reference implemented

--- evaluation/attribution_methods_evaluator.py
+++ evaluation/attribution_methods_evaluator.py
@@ -11,6 +11,8 @@ from data.util.utils import DatasetMode
 from model.attribution_methods.integrated_gradients import IntegratedGradients
 from evaluation.utils.visualisation import _visualize_log_odds
 
+import copy
+
 
 
 class AttributionMethodsEvaluator():
@@ -28,6 +30,7 @@ class AttributionMethodsEvaluator():
             self,
             x: torch.Tensor,
             attribute,
+            apply_log: bool = True,
             **kwargs) -> np.ndarray:
         """
         Calculates the log odds of one datapoint. Designed generically so it can work with different attribution methods. To work with an attribution method,
@@ -46,12 +49,12 @@ class AttributionMethodsEvaluator():
                     attribution_scores (torch.Tensor)
                     target_label_index (int): Equals the input argument, except the input argument is None. In this case, the
                         index of the maximum output feature is returned.
-            baseline (torch.Tensor): baseline for the Integrated Gradient Method. Does only need to be given if this method is used.
+            apply_log (bool, optional): If the log should be applied: log(certainty / 1-certainty).
             **kwargs: additional arguments for specific attribution methods, e.g. baseline for integrated gradients. **kwargs get passed
                 to the attribute callable.
 
         Returns:
-            log_odds_of_datapoint(np.ndarray): Log odds of the given input.
+            certainties or log_odds_of_datapoint(np.ndarray): Depending on apply_log
         """
 
         attribution_scores, target_label_index = attribute(x,target_label_index=None, **kwargs)
@@ -65,17 +68,60 @@ class AttributionMethodsEvaluator():
             prediction = self.model.predict(x)
             predictions_with_mask[i] = prediction[target_label_index]
 
+        if apply_log:
+            counter_propability = 1 - predictions_with_mask
+            log_odds = np.log(predictions_with_mask + 1**(-16) / counter_propability)
+            return log_odds
+        else:
+            return predictions_with_mask
+    
+    def get_random_references_of_datapoint(
+            self,
+            x: torch.Tensor,
+            apply_log : bool = True,
+            ) -> np.ndarray:
+        """
+        Calculates a random reference of a datapoint by randomly choosing the masking order.
+
+        Args:
+            x (torch.Tensor): datapoint for which the reference gets calculated.
+            apply_log (bool, optional): If the log should be applied: log(certainty / 1-certainty).
+
+        Returns:
+            certainties or log_odds
+        """
         
-        counter_propability = 1 - predictions_with_mask
-        log_odds = np.log(predictions_with_mask + 1**(-16) / counter_propability)
-        return log_odds
+        target_label_index = self.model.predict(x).argmax().item()
+        # print("Model prediction: " + str(self.model.predict(x)))
+        random_masking_order = np.random.choice(a=16, size=16, replace=False)
+        
+        predictions_with_random_mask = np.zeros((16))
+        # print("initial x: " + str(x))
+
+        for i in range(len(predictions_with_random_mask)):
+            x[random_masking_order[0:i]] = 0
+            # print("Masked input: " + str(x))
+            prediction = self.model.predict(x)
+            # print("Target-label-index: " + str(target_label_index))
+            predictions_with_random_mask[i] = prediction[target_label_index]
+            # print("Prediction: " + str(prediction))
+
+        if apply_log:
+            counter_propability = 1 - predictions_with_random_mask
+            log_odds = np.log(predictions_with_random_mask + 1**(-16) / counter_propability)
+            return log_odds
+        
+        else:
+            return predictions_with_random_mask
+
     
     def get_log_odds_of_dataset(
             self,
             dataset: torch.utils.data.Dataset,
             attribute,
+            apply_log: bool = True,
             **kwargs
-        ) -> [np.ndarray, #log-odds
+        ) -> tuple[np.ndarray, #log-odds
               np.ndarray, #mean of log_odds
               np.ndarray, #max of log_odds
               np.ndarray]:  #min of log_odds
@@ -98,14 +144,14 @@ class AttributionMethodsEvaluator():
                     attribution_scores (torch.Tensor)
                     target_label_index (int): Equals the input argument, except the input argument is None. In this case, the
                         index of the maximum output feature is returned.
-            baseline (torch.Tensor): baseline for the Integrated Gradient Method. Does only need to be given if this method is used.
+            apply_log (bool, optional): If the log should be applied: log(certainty / 1-certainty).
             **kwargs: additional arguments for specific attribution methods, e.g. baseline for integrated gradients. **kwargs get passed
                 to the attribute callable.
         """
         log_odds = np.zeros((len(dataset),16))
 
         for i in tqdm(range(len(log_odds))):
-            log_odds[i] = self.get_log_odds_of_datapoint(dataset[i][0],attribute=attribute,**kwargs)
+            log_odds[i] = self.get_log_odds_of_datapoint(dataset[i][0],attribute=attribute,apply_log=apply_log,**kwargs)
 
         #mean, max and min calculation
         mean = log_odds.mean(axis=0)
@@ -121,11 +167,41 @@ class AttributionMethodsEvaluator():
 
 
         return log_odds, mean, max, min
+    
+    def get_random_references_of_dataset(
+            self,
+            dataset: torch.utils.data.Dataset,
+            apply_log : bool = True
+        ) -> tuple[np.ndarray, np.ndarray] :
+        """
+        Calculates a random reference of each datapoint in a dataset and the mean of them. The masking order is choosen ramdomly for each datapoint. 
+            Designed generically so it can work with different attribution methods. To work with an attribution method,
+            it needs to implement the attribute callable (see args below).
+
+        Args:
+            dataset (torch.utils.data.Dataset): Dataset for which the baseline gets calculated.
+            apply_log (bool, optional): If the log should be applied: log(certainty / 1-certainty).
+
+        Returns:
+            random references (np.ndarray)
+            mean (np.ndarray): mean of references
+        """
+
+        random_references = np.zeros((len(dataset), 16))
+
+        for i in tqdm(range(len(random_references))):
+            random_references[i] = self.get_random_references_of_datapoint(dataset[i][0], apply_log=apply_log)
+            
+
+        mean = random_references.mean(axis=0)
+
+        return random_references, mean
 
     def visualize_log_odds_of_dataset(
             self,
             dataset: torch.utils.data.Dataset,
             attribute,
+            apply_log: bool = True,
             **kwargs
         ) -> None:
         """
@@ -151,6 +227,10 @@ class AttributionMethodsEvaluator():
                 to the attribute callable.
         """
 
-        log_odds, mean, max, min = self.get_log_odds_of_dataset(dataset,attribute,**kwargs)
+        dataset_copy = copy.deepcopy(dataset)
+        log_odds, mean, max, min = self.get_log_odds_of_dataset(dataset_copy,attribute,apply_log,**kwargs)
 
-        _visualize_log_odds(log_odds, mean, max, min)
+        dataset_copy = copy.deepcopy(dataset)
+        random_references, random_references_mean = self.get_random_references_of_dataset(dataset=dataset_copy,apply_log=apply_log)
+
+        _visualize_log_odds(log_odds, mean, max, min, random_references_mean,apply_log)
